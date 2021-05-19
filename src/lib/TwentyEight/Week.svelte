f79e96e310994e5773b39b7eca82b37a4d7b6162
Brent Labasan <BrentLabasan@gmail.com>
2021-05-18 23:26:21 -0700

    css grid

---
+++ src/lib/TwentyEight/Week.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Day from './Day.svelte';
+	const timesToBeRendered = 7;
+</script>
+
+<section>
+
+	{#each Array(timesToBeRendered) as _, index}
+		<Day />
+	{/each}
+</section>
+
+<style>
+	section {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+	}
+</style>
--- src/lib/TwentyEight/index.svelte
+++ src/lib/TwentyEight/index.svelte
@@ -1,3 +1,11 @@
+<script>
+	import Week from './Week.svelte';
+	const timesToBeRendered = 4;
+</script>
+
 <section>
-    a TwentyEight
+	a TwentyEight
+	{#each Array(timesToBeRendered) as _, index}
+		<Week />
+	{/each}
 </section>
